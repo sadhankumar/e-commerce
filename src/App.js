18b2ad998727ca38cbbdd8a5a8fffe6ca664f9a2
Sadhan Kumar <sadhan.kumar@borngroup.com>
2019-12-09 19:46:10 +0530

    preview-page add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import {Switch, Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components.jsx';
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <HomePage />
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
       </div>
     );
   }
