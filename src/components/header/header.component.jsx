463667e2312a659e2f8490813e985805d9e5f662
Sadhan Kumar <sadhan.kumar@borngroup.com>
2019-12-12 20:15:58 +0530

    cart-item add using redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -24,8 +28,16 @@ const Header = ({ currentUser }) => (
         :
         <Link className='option' to='/signin'> SIGN IN </Link>
       }
-    </div>
+      <CartIcon />
+    </div>{
+    hidden ? null :  <CartDropdown/>
+    }
   </div>
-)
+);
 
-export default Header;
+const mapStateTopProps = ({user: { currentUser}, cart: { hidden} }) => ({
+  currentUser,
+  hidden
+})
+
+export default connect(mapStateTopProps)(Header);
